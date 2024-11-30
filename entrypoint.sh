85e757eb0fc638c639f5211fa0e4d09c16c6a9da
oksii <oksii@duck.com>
2024-11-30 17:49:52 +0100

    fix reqss

--- entrypoint.sh
+++ entrypoint.sh
@@ -27,6 +27,9 @@ declare -A CONFIG=(
     [AUTO_UPDATE]="true"
     [XMAS]="false"
     [XMAS_FILE]="http://rtcw.life/files/mapdb/mp_gathermas.pk3"
+    [CONF_SSADDRESS]=""
+    [CONF_SSWEBHOOKID]=""
+    [CONF_SSWEBHOOKTOKEN]=""
 )
 
 # Load environment variables into config
